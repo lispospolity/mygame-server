e6db04b92e09a114072092dfc2df0f9ba2886e59
lispospolity <lis.posibility@gmail.com>
2026-07-03 21:50:08 +0200

    speed state

--- Game/src/main/java/Entity.java
+++ Game/src/main/java/Entity.java
@@ -2,6 +2,7 @@ import java.sql.SQLException;
 import java.util.HashMap;
 
 public class Entity {
+    static int defaultSpeed = 1;
     static HashMap<String, state> entmap = new HashMap<String, state>();
     static Websocket instance = Websocket.getInstance();
     static PlayerStates playerstates;
@@ -21,7 +22,7 @@ public class Entity {
         message.put("x", ""+coords.x());
         message.put("y", ""+coords.y());
         //message building ^
-        entmap.put(name, new state(coords.x(),coords.y()));
+        entmap.put(name, new state(coords.x(),coords.y(), defaultSpeed));
         instance.broadcastWS("200", message);
     }
     public static void delEntity(String name, state state) throws SQLException {
@@ -35,8 +36,8 @@ public class Entity {
         //remove from walking delay hashmap
         PlayerMovement.walkAvailableTime.remove(name);
     }
-    public record state(int x, int y) {}
+    public record state(int x, int y, int speedMultiplier) {}
 }
-//TODO implement new state - speed and combine it with PlayerMovement
+//TODO add hotbar and events for swapping weapons in it (add it to state too)
 //TODO add health to state and class health regen with a scheduler
 //TODO change name to Player and make an Enemy or Entity class (Entity better)
--- Game/src/main/java/PlayerMovement.java
+++ Game/src/main/java/PlayerMovement.java
@@ -9,7 +9,7 @@ public class PlayerMovement {
         if (walkingDelay(name)) return;
         Entity.state coords = Entity.entmap.get(name);
         if (world[coords.x()][coords.y()-1] == 1) return;
-        Entity.entmap.replace(name, new Entity.state(coords.x(), coords.y()-1));
+        Entity.entmap.replace(name, new Entity.state(coords.x(), coords.y()-1, Entity.entmap.get(name).speedMultiplier()));
         //message building and sending
         HashMap<String, String> message = new HashMap<String, String>();
         message.put("unm", name);
@@ -21,7 +21,7 @@ public class PlayerMovement {
         if (walkingDelay(name)) return;
         Entity.state coords = Entity.entmap.get(name);
         if (world[coords.x()][coords.y()+1] == 1) return;
-        Entity.entmap.replace(name, new Entity.state(coords.x(), coords.y()+1));
+        Entity.entmap.replace(name, new Entity.state(coords.x(), coords.y()+1, Entity.entmap.get(name).speedMultiplier()));
         //message building and sending
         HashMap<String, String> message = new HashMap<String, String>();
         message.put("unm", name);
@@ -33,7 +33,7 @@ public class PlayerMovement {
         if (walkingDelay(name)) return;
         Entity.state coords = Entity.entmap.get(name);
         if (world[coords.x()-1][coords.y()] == 1) return;
-        Entity.entmap.replace(name, new Entity.state(coords.x()-1, coords.y()));
+        Entity.entmap.replace(name, new Entity.state(coords.x()-1, coords.y(), Entity.entmap.get(name).speedMultiplier()));
         //message building and sending
         HashMap<String, String> message = new HashMap<String, String>();
         message.put("unm", name);
@@ -45,7 +45,7 @@ public class PlayerMovement {
         if (walkingDelay(name)) return;
         Entity.state coords = Entity.entmap.get(name);
         if (world[coords.x()+1][coords.y()] == 1) return;
-        Entity.entmap.replace(name, new Entity.state(coords.x()+1, coords.y()));
+        Entity.entmap.replace(name, new Entity.state(coords.x()+1, coords.y(), Entity.entmap.get(name).speedMultiplier()));
         //message building and sending
         HashMap<String, String> message = new HashMap<String, String>();
         message.put("unm", name);
@@ -54,11 +54,15 @@ public class PlayerMovement {
         instance.broadcastWS("202", message);
     }
     private static Boolean walkingDelay(String name) {
+
         if (!walkAvailableTime.containsKey(name)) {
             walkAvailableTime.put(name, System.currentTimeMillis());
         }
         if (walkAvailableTime.get(name)<System.currentTimeMillis()) {
-            walkAvailableTime.put(name, System.currentTimeMillis()+200);
+            float speed = Entity.entmap.get(name).speedMultiplier();
+            float fltDelay = 200/speed;
+            int delay = (int)fltDelay;
+            walkAvailableTime.put(name, System.currentTimeMillis()+delay);
             return false;
         }
         return true;
